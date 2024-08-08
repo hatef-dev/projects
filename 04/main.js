3ce6e21a93f1f256713fce63295dee24c384f546
hatef-dev <hatef.dev21@gmail.com>
2024-08-08 20:41:53 +0330

    add animation

--- 04/main.js
+++ 04/main.js
@@ -1,24 +1,66 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import barba from "@barba/core";
+import { gsap } from "gsap";
+const tlLeave = gsap.timeline({
+  defaults: { duration: 0.75, ease: "power3.out" },
+});
+const tlEnter = gsap.timeline({
+  defaults: { duration: 0.75, ease: "power3.out" },
+});
+const leaveAnimation = (current, done) => {
+  const product = current.querySelector(".image-container");
+  const text = current.querySelector(".showcase-text");
+  
+  const circle = current.querySelectorAll(".circle");
+  const arrow = current.querySelector(".showcase-arrow");
+  console.log(arrow)
+  return (
+    tlLeave.fromTo(arrow, { opacity: 1, y: 0 }, { opacity: 0, y: 50 }),
+    tlLeave.fromTo(product, { opacity: 1, y: 0 }, { opacity: 0, y: 100, onComplete: done}, "<"),
+    tlLeave.fromTo(
+      text,
+      { y: 0, opacity: 1 },
+      { opacity: 0, y: -100,  },
+      "<"
+    ),
+    tlLeave.fromTo(circle, {y: 0, opacity: 1}, {y: -200, opacity: 0,  }, "<")
+  );
+};
+const enterAnimation = (current, done) => {
+  const product = current.querySelector(".image-container");
+  const text = current.querySelector(".showcase-text");
+  const circle = current.querySelectorAll(".circle");
+  const arrow = current.querySelector(".showcase-arrow");
+  console.log(arrow)
+  return (
+    tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+    tlLeave.fromTo(product, { opacity: 0, y: 100 }, { opacity:1, y: 0, onComplete: done}, "<"),
+    tlLeave.fromTo(
+      text,
+      { y: -100, opacity: 0 },
+      { opacity:1, y: 0,  },
+      "<"
+    ),
+    tlLeave.fromTo(circle, {y: -200, opacity: 0}, {y: 0, opacity: 1, stagger: 0.15, }, "<")
+  );
+};
+barba.init({
+  preventRunning: true,
+  transitions: [
+    {
+      name: "default",
+      leave(data) {
+        // create your stunning leave animation here
+        const done = this.async();
+        let currentPage = data.current.container;
+        console.log(currentPage)
+        leaveAnimation(currentPage, done);
+      },
+      enter(data) {
+        // create your amazing enter animation here
+        const done = this.async();
+        let next = data.next.container;
+        enterAnimation(next, done)
+      },
+    },
+  ],
+});
